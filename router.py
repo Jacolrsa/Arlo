da1828ad440a6f103cbca13eddd0e2460be7b809
Jacolrsa <jacolrsa@gmail.com>
2026-07-03 10:40:17 +0200

    Remove leaderboard command

--- router.py
+++ router.py
@@ -1,10 +1,8 @@
 from .commands import help
-from .commands import leaderboard
 from .commands import meshcoremonday
 
 DIRECT_COMMANDS = {
     help.COMMAND: help,
-    leaderboard.COMMAND: leaderboard,
     meshcoremonday.COMMAND: meshcoremonday,
 }
 
@@ -15,16 +13,7 @@ async def handle_message(ctx):
     if ctx.sender == "Arlo":
         return
 
-    if ctx.message_type == "direct":
-        command = DIRECT_COMMANDS.get(ctx.message.strip().lower())
+    command = DIRECT_COMMANDS.get(ctx.message.strip().lower())
 
-        if command:
-            await command.execute(ctx)
-        return
-
-    if ctx.message_type == "channel":
-        command = DIRECT_COMMANDS.get(ctx.message.strip().lower())
-
-        if command:
-            await command.execute(ctx)
-        return
+    if command:
+        await command.execute(ctx)
